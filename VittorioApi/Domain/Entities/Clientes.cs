7eff23254ae8cf6bf6e97d99c9d615adfe5eab2c
vittorio <vittfassano@gmail.com>
2024-08-15 11:29:04 -0300

    fix: modificando migrations

--- VittorioApi/Domain/Entities/Clientes.cs
+++ VittorioApi/Domain/Entities/Clientes.cs
@@ -5,8 +5,6 @@ namespace VittorioApiT2M.Domain.Entities
         public required int Id { get; set; }
         public required string Nome { get; set; }
         public required string Email { get; set; }
-        public required string Senha { get; set; }
-
         public ICollection<Reservas> Reserva { get; set; } = [];
     }
 }
--- VittorioApi/Infrastructure/Migrations/V1__Create&Insert_Cliente_and_Reservas_Tables.sql
+++ VittorioApi/Infrastructure/Migrations/V1__Create&Insert_Cliente_and_Reservas_Tables.sql
@@ -2,8 +2,7 @@
 CREATE TABLE Cliente (
     Id SERIAL PRIMARY KEY,
     Nome VARCHAR(255) NOT NULL,
-    Email VARCHAR(255) NOT NULL UNIQUE,
-    Senha VARCHAR(255) NOT NULL
+    Email VARCHAR(255) NOT NULL UNIQUE
 );
 
 -- Criação da tabela Reservas
@@ -17,17 +16,17 @@ CREATE TABLE Reservas (
 );
 
 -- Inserção de registros na tabela Cliente
-INSERT INTO Cliente (Nome, Email, Senha) VALUES
-('Paulo Hernique', 'paulo.hernique@example.com', '123'),
-('Thiago Silva', 'thiago.silva@example.com', '123');
+INSERT INTO Cliente (Nome, Email) VALUES
+('Paulo Hernique', 'paulo.hernique@example.com'),
+('Thiago Silva', 'thiago.silva@example.com');
 
 -- Inserção de registros na tabela Reservas
--- Reservas para João da Silva (ClienteId = 1)
+-- Reservas para Paulo Hernique (ClienteId = 1)
 INSERT INTO Reservas (ClienteId, DataReserva, HoraReserva, NumeroPessoas, Confirmada) VALUES
 (1, '2024-08-15', '19:00:00', 4, TRUE),
 (1, '2024-08-25', '20:30:00', 2, FALSE);
 
--- Reservas para Maria Oliveira (ClienteId = 2)
+-- Reservas para Thiago Silva (ClienteId = 2)
 INSERT INTO Reservas (ClienteId, DataReserva, HoraReserva, NumeroPessoas, Confirmada) VALUES
 (2, '2024-08-10', '18:00:00', 3, TRUE),
 (2, '2024-08-20', '21:00:00', 5, TRUE);
